4a65172e689eed9583a2f4d3c368aec66662ecfe
Srikanth5253 <srikanth.boda@motivitylabs.com>
2026-05-20 12:19:40 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -19,16 +19,11 @@ import errorHandler from "./middleware/errorMiddleware.js";
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
-// app.use(cors({
-//   origin:[
-//   process.env.CLIENT_URL,
-//   process.env.LocalHost_CLIENT_URL,
-//   ],
-//   credentials: true
-// }));
+app.use(cors({
+  origin:"*",
+}));
 
 app.use("/api/auth", authRoutes);
 app.use(
