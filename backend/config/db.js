fa84e3d5284b4df4e111fb4748ab985688f88eee
Srikanth5253 <srikanth.boda@motivitylabs.com>
2026-05-20 13:50:33 +0530

    Done with atlas

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,30 @@
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI);
+//     console.log("MongoDB Connected");
+//   } catch (error) {
+//     console.error("DB Error:", error.message);
+//     process.exit(1);
+//   }
+// };
+
+// export default connectDB;
+
 import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Connected");
+    const conn = await mongoose.connect(
+      process.env.MONGO_URI
+    );
+
+    console.log(
+      `MongoDB Connected: ${conn.connection.host}`
+    );
   } catch (error) {
-    console.error("DB Error:", error.message);
+    console.log(error);
     process.exit(1);
   }
 };
