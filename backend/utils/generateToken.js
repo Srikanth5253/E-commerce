3cb1d879b68f62e8699a51e3f8ebb8e64c7fa0c7
Srikanth5253 <srikanth.boda@motivitylabs.com>
2026-05-27 16:46:00 +0530

    updated

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,17 +1,3 @@
-// import jwt from "jsonwebtoken";
-
-// const generateToken = (id, role) => {
-//   return jwt.sign(
-//     { id, role },
-//     process.env.JWT_SECRET,
-//     {
-//       expiresIn: "7d",
-//     }
-//   );
-// };
-
-// export default generateToken;
-
 import jwt from "jsonwebtoken";
 
 const generateToken = (id) => {
